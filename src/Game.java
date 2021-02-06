a06b35c58bda49c1192d8783b2f7325d154ef9a9
adarSonmez <adarsonmez@outlook.com>
2021-02-06 19:42:19 +0300

    update Game.java

--- src/Game.java
+++ src/Game.java
@@ -1,57 +1,93 @@
+import javax.swing.*;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Game {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
+        createRulesFile();
         // Get a country randomly
         String[] data = getData();
-        String country = data[(int)(Math.random() * data.length)];
-        Scanner input = new Scanner(System.in);
-
-        char[] word = toArray(country);
-        char[] hiddenLetters =  hideLetters(word);
-        ArrayList<String> letters = new ArrayList<>();
+        String country = data[(int) (Math.random() * data.length)];
 
         // Introduce the game
-        System.out.println("WELCOME TO THE HANGMAN GAME.");
-        System.out.println("This is a country.");
-        System.out.println("You can have up to " + (country.length() / 4 + 7) + " letters.");
-        System.out.println("Be careful! You only have one chance to guess the word.");
-        System.out.println("Please use dashes(-) instead of spaces!");
-        System.out.print("let's start by having a letter: ");
+        String letter = JOptionPane.showInputDialog(
+                null,
+                "WELCOME TO THE HANGMAN GAME. \n" +
+                        "This is a country. \n" +
+                        "You can have up to " + (country.length() / 4 + 7) + " letters. \n" +
+                        "Let's start by having a letter: "
+        );
+
+        char[] word = toArray(country);
+        char[] hiddenLetters = hideLetters(word);
+        ArrayList<String> letters = new ArrayList<>();
 
         // Determine how many letters can user takes
         int rights = (country.length() / 4 + 6);
-        String letter = input.next();
         SearchForLetter(letter, word, hiddenLetters);
 
         // Game Process
         for (int i = 0; i < (country.length() / 4 + 7); i++, rights--) {
-            // stack letters user entered
+            // Stack letters user entered
             if (letters.contains(letter)) {
-                System.out.println("You already entered " + letter);
+                JOptionPane.showMessageDialog(
+                        null,
+                        "You already entered " + letter,
+                        "Message",
+                        JOptionPane.INFORMATION_MESSAGE
+                );
+                rights++;
                 i--;
             } else {
                 letters.add(letter);
             }
 
+            // This is your last chance
             if (rights == 0) {
                 SearchForLetter(letter, word, hiddenLetters);
-                System.out.println(hiddenLetters);
             }
 
             // Enable user to guess the word
             if (letter.equals("0")) {
-                System.out.print("Write your guess for this country: ");
-                String guess = input.next().toLowerCase();
+                String guess = JOptionPane.showInputDialog(
+                        null,
+                        "Write your guess for this country: \n" +
+                                format(hiddenLetters),
+                        "Guess",
+                        JOptionPane.QUESTION_MESSAGE
+                ).toLowerCase();
                 checkTheGuess(guess, word);
 
                 letter = "-1"; // If the guess is wrong
             } else {
                 // Prompt user to enter a letter
-                enterALetter(hiddenLetters, rights, word);
-                letter = input.next();
+                letter = enterALetter(hiddenLetters, rights, word);
                 SearchForLetter(letter, word, hiddenLetters);
+            }
+        }
+    }
+
+    public static void createRulesFile() throws FileNotFoundException {
+        File rules = new File("../Hangman/rules.txt");
+        if (rules.exists()) {
+            // when you use try-with source syntax, you don't have to write "output.close"
+            try (PrintWriter output = new PrintWriter(rules)) {
+                output.println("RULES!!");
+                output.println("Welcome to the hangman game!");
+                output.println("This a command-line-game. You need to use a JAVA IDE to play");
+                output.println("We will choose a country from our world and you will try to guess it by taking letters.");
+                output.println("You can only have a certain number of letters.");
+                output.println("As the number of words in the country increases, we will give you the right to take more letters");
+                output.println("Be careful! You only have one chance to guess the word.");
+                output.println("PLEASE USE DASHES(-) INSTEAD OF SPACES!");
+                output.println("Now you are ready to play the game.");
+                output.println("Have Fun!");
+                output.println();
+                output.print("https://github.com/adarSonmez/hangman-game");
+                // output.close(); is redundant)
             }
         }
     }
@@ -62,7 +98,7 @@ public class Game {
         for (int i = 0; i < country.length(); i++) {
             if (country.charAt(i) == ' ') {
                 // Convert spaces to dashes
-                letters[i] = '-';
+                letters[i] = ' ';
                 continue;
             }
             letters[i] = country.charAt(i);
@@ -96,25 +132,47 @@ public class Game {
     }
 
     // Prompt user to enter a letter
-    public static void enterALetter(char[] wordArr, int rights, char[] word) {
+    public static String enterALetter(char[] wordArr, int rights, char[] word) {
+        StringBuilder wordStr = format(wordArr);
+
+        if (rights == 1) {
+            return  JOptionPane.showInputDialog(
+                    null,
+                    "You can have one more letter for " + wordStr +
+                            "\n or enter 0 to guess" ,
+                    "Input",
+                    JOptionPane.QUESTION_MESSAGE
+            );
+        } else if (rights != 0) {
+            return  JOptionPane.showInputDialog(
+                    null,
+                    "Enter a latter in word " + wordStr +
+                            "\n or enter 0 to guess" ,
+                    "Input",
+                    JOptionPane.QUESTION_MESSAGE
+            );
+        } else {
+            // The last chance to win the game
+            String guess = JOptionPane.showInputDialog(
+                    null,
+                    wordStr +
+                            "Write your guess for this country ",
+                    "Guess",
+                    JOptionPane.QUESTION_MESSAGE
+            ).toLowerCase();
+            checkTheGuess(guess, word);
+        }
+        return "";
+    }
+
+    public static StringBuilder format(char[] wordArr) {
         StringBuilder wordStr = new StringBuilder();
-        Scanner input = new Scanner(System.in);
 
         // Convert array country to a string
         for (char e : wordArr) {
             wordStr.append(e);
         }
-
-        if (rights == 1) {
-            System.out.print("You can have one more letter for " + wordStr + " or enter 0 to guess > ");
-        } else if (rights != 0) {
-            System.out.print("Enter a latter in word " + wordStr + " or enter 0 to guess > ");
-        } else {
-            // The last chance to win the game
-            System.out.print("Write your guess for country above: ");
-            String guess = input.next().toLowerCase();
-            checkTheGuess(guess, word);
-        }
+        return wordStr;
     }
 
     // Check user's guess
@@ -127,9 +185,9 @@ public class Game {
             guessArr[i] = guess.charAt(i);
         }
 
-        StringBuilder ansStr = new StringBuilder();
+        StringBuilder ansStrBuilder = new StringBuilder();
         for (char c : answer) {
-            ansStr.append(c);
+            ansStrBuilder.append(c);
         }
 
         // Is user's guess correct?
@@ -141,264 +199,52 @@ public class Game {
             }
         }
 
+        String ansStr = ansStrBuilder.toString().toUpperCase();
+
         if (guessArr.length != answer.length) {
-            System.out.println("Pay attention to the number of the letters!");
-            System.out.println("The correct answer was " + ansStr);
-            System.out.println("You lose.");
+            JOptionPane.showMessageDialog(
+                    null,
+                    "Pay attention to the number of the letters! \n" +
+                            "The correct answer was '" + ansStr + " " +
+                            "'\nYou lose.",
+                    "You Lose!",
+                    JOptionPane.PLAIN_MESSAGE
+            );
         } else if (win) {
-            System.out.println("Congratulations! " + ansStr + " is the correct answer.");
-            System.out.println("Are you Google Maps?");
+            JOptionPane.showMessageDialog(
+                    null,
+                    "Congratulations! '" + ansStr + "' is the correct answer.! \n" +
+                            "Are you Google Maps?",
+                    "You Won!",
+                    JOptionPane.PLAIN_MESSAGE
+            );
         } else {
-            System.out.println("HAHAHAHA!");
-            System.out.println("The correct answer was " + ansStr);
-            System.out.println("You're a loser :D");
+            JOptionPane.showMessageDialog(
+                    null,
+                    "HAHAHAHA! \n" +
+                            "The correct answer was \n'" +
+                            ansStr + " " +
+                            "'You're a loser :D \n",
+                    "You Lose!",
+                    JOptionPane.PLAIN_MESSAGE
+            );
         }
         System.exit(0);
     }
 
     // All countries (I hope..)
-    public static String[] getData() {
-        return new String[]{
-                "afghanistan",
-                "albania",
-                "algeria",
-                "american samoa",
-                "andorra",
-                "angola",
-                "anguilla",
-                "antarctica",
-                "antigua and barbuda",
-                "argentina",
-                "armenia",
-                "aruba",
-                "australia",
-                "austria",
-                "azerbaijan",
-                "bahamas",
-                "bahrain",
-                "bangladesh",
-                "barbados",
-                "belarus",
-                "belgium",
-                "belize",
-                "benin",
-                "bermuda",
-                "bhutan",
-                "bolivia",
-                "bosnia and herzegovina",
-                "botswana",
-                "bouvet island",
-                "brazil",
-                "british indian ocean territory",
-                "brunei darussalam",
-                "bulgaria",
-                "burkina faso",
-                "burundi",
-                "cambodia",
-                "cameroon",
-                "canada",
-                "cape verde",
-                "cayman islands",
-                "central african republic",
-                "chad",
-                "chile",
-                "china",
-                "christmas island",
-                "cocos islands",
-                "colombia",
-                "comoros",
-                "congo",
-                "the democratic republic of the congo",
-                "cook islands",
-                "costa rica",
-                "cote d'Ivoire",
-                "croatia",
-                "cuba",
-                "cyprus",
-                "czech republic",
-                "denmark",
-                "djibouti",
-                "dominica",
-                "dominican republic",
-                "east timor",
-                "ecuador",
-                "egypt",
-                "el salvador",
-                "equatorial guinea",
-                "eritrea",
-                "estonia",
-                "ethiopia",
-                "falkland islands",
-                "faroe islands",
-                "fiji",
-                "finland",
-                "france",
-                "france metropolitan",
-                "french guiana",
-                "french polynesia",
-                "french southern territories",
-                "gabon",
-                "gambia",
-                "georgia",
-                "germany",
-                "ghana",
-                "gibraltar",
-                "greece",
-                "greenland",
-                "grenada",
-                "guadeloupe",
-                "guam",
-                "guatemala",
-                "guinea",
-                "guinea-bissau",
-                "guyana",
-                "haiti",
-                "heard and mc donald islands",
-                "holy see",
-                "honduras",
-                "hong kong",
-                "hungary",
-                "iceland",
-                "india",
-                "indonesia",
-                "iran",
-                "iraq",
-                "ireland",
-                "israel",
-                "italy",
-                "jamaica",
-                "japan",
-                "jordan",
-                "kazakhstan",
-                "kenya",
-                "kiribati",
-                "democratic people's republic of korea",
-                "republic of korea",
-                "kuwait",
-                "kyrgyzstan",
-                "people's democratic republic lao",
-                "latvia",
-                "lebanon",
-                "lesotho",
-                "liberia",
-                "libyan arab jamahiriya",
-                "liechtenstein",
-                "lithuania",
-                "luxembourg",
-                "macau",
-                "the former yugoslav republic of macedonia",
-                "madagascar",
-                "malawi",
-                "malaysia",
-                "maldives",
-                "mali",
-                "malta",
-                "marshall islands",
-                "martinique",
-                "mauritania",
-                "mauritius",
-                "mayotte",
-                "mexico",
-                "Federated states of micronesia",
-                "republic of moldova",
-                "monaco",
-                "mongolia",
-                "montserrat",
-                "morocco",
-                "mozambique",
-                "myanmar",
-                "namibia",
-                "nauru",
-                "nepal",
-                "netherlands",
-                "netherlands antilles",
-                "new caledonia",
-                "new zealand",
-                "nicaragua",
-                "niger",
-                "nigeria",
-                "niue",
-                "norfolk island",
-                "northern mariana islands",
-                "norway",
-                "oman",
-                "pakistan",
-                "palau",
-                "panama",
-                "papua new guinea",
-                "paraguay",
-                "peru",
-                "philippines",
-                "pitcairn",
-                "poland",
-                "portugal",
-                "puerto rico",
-                "qatar",
-                "reunion",
-                "romania",
-                "russian federation",
-                "rwanda",
-                "saint kitts and nevis",
-                "saint lucia",
-                "saint vincent and the grenadines",
-                "samoa",
-                "san marino",
-                "sao tome and principe",
-                "saudi arabia",
-                "senegal",
-                "seychelles",
-                "sierra leone",
-                "singapore",
-                "slovakia",
-                "slovenia",
-                "solomon islands",
-                "somalia",
-                "south africa",
-                "south georgia and the south sandwich islands",
-                "spain",
-                "sri lanka",
-                "st. helena",
-                "st. pierre and miquelon",
-                "state of palestine",
-                "sudan",
-                "suriname",
-                "svalbard and jan mayen islands",
-                "swaziland",
-                "sweden",
-                "switzerland",
-                "syrian arab republic",
-                "taiwan",
-                "tajikistan",
-                "united republic of tanzania",
-                "thailand",
-                "togo",
-                "tokelau",
-                "tonga",
-                "trinidad and tobago",
-                "tunisia",
-                "turkey",
-                "turkmenistan",
-                "turks and caicos islands",
-                "tuvalu",
-                "uganda",
-                "ukraine",
-                "united arab emirates",
-                "united kingdom",
-                "united states",
-                "united states minor outlying islands",
-                "uruguay",
-                "uzbekistan",
-                "vanuatu",
-                "venezuela",
-                "vietnam",
-                "british virgin islands",
-                "virgin islands of the united states",
-                "wallis and futuna islands",
-                "western sahara",
-                "yemen",
-                "yugoslavia",
-                "zambia",
-                "zimbabwe"
-        };
+    public static String[] getData() throws FileNotFoundException {
+        ArrayList<String> myList = new ArrayList<>();
+        File file = new File("../Hangman/countries.txt");
+        Scanner c = new Scanner(file);
+        // data.useDelimiter();
+
+        while (c.hasNext()) {
+            myList.add(c.nextLine());
+        }
+        c.close();
+
+        String[] data = new String[myList.size()];
+        return myList.toArray(data);
     }
 }
